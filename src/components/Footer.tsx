e5ec49b85bf020d14141ac62142d27dca3c02396
Jone <duc66960@gmail.com>
2025-10-03 14:44:29 +0700

    update

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black text-white text-center'>
+
+        Copyright &copy :Sick <br />
+        All Rights Reserved 2024
+    </div>
+  )
+}
+
+export default Footer
